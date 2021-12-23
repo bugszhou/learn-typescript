ff4a55d0e2e1a752c0b69d80b90d58ea6708abb9
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-12-23 17:59:02 +0800

    feat: 增加symbol

---
+++ __tests__/lesson_1/symbol.test.ts
@@ -0,0 +1,46 @@
+describe("基础类型: symbol", () => {
+  it("不同描述符", () => {
+    let symbol1: symbol;
+    let symbol2: symbol;
+
+    symbol1 = Symbol();
+    symbol2 = Symbol("demo");
+
+    expect(symbol1 === symbol2).toBeFalsy();
+  });
+
+  it("相同描述符", () => {
+    let symbol1: symbol;
+    let symbol2: symbol;
+
+    symbol1 = Symbol("demo");
+    symbol2 = Symbol("demo");
+
+    expect(symbol1 === symbol2).toBeFalsy();
+  });
+
+  it("唯一标识符", () => {
+    const symbol1 = Symbol("demo");
+    const symbol2 = Symbol("demo");
+
+    const obj: any = {};
+    obj[symbol1] = "test";
+    obj[symbol2] = "test1";
+
+    expect(obj[symbol1]).toBe("test");
+    expect(obj[symbol2]).toBe("test1");
+  });
+
+  it("全局symbol", () => {
+    const symbol1 = Symbol.for("demo"); // 全局没有描述符为demo的symbol值时，创建symbol值，有则返回
+    const symbol2 = Symbol.for("demo");
+
+    const obj: any = {};
+    obj[symbol1] = "test";
+    obj[symbol2] = "test1";
+
+    expect(obj[symbol1]).toBe("test");
+    expect(obj[symbol1]).toBe("test1");
+    expect(obj[symbol2]).toBe("test1");
+  });
+});
